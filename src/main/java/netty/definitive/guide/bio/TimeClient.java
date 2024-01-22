cd232f850815d5e9a44934a5434d84e595693e5d
abing22333 <abing22333@163.com>
2024-01-22 18:00:42 +0800

    commit: 提交代码

---
+++ src/main/java/netty/definitive/guide/bio/TimeClient.java
@@ -0,0 +1,13 @@
+package netty.definitive.guide.bio;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TimeClient {
+
+
+    public static void main(String[] args) {
+        int port = 18080;
+
+
+    }
+}
---
+++ src/main/java/netty/definitive/guide/bio/TimeServerHandler.java
@@ -0,0 +1,40 @@
+package netty.definitive.guide.bio;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Date;
+
+public class TimeServerHandler implements Runnable {
+    Socket socket;
+
+    public TimeServerHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+
+            while (true) {
+                String body = in.readLine();
+                if (body == null) {
+                    break;
+                }
+                System.out.println("The time server receive order: " + body);
+                String currentTime = "QUERY TIME ORDER".equals(body) ? new Date().toString() : "BAD ORDER";
+                out.println(currentTime);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            this.socket = null;
+        }
+    }
+
+
+}
